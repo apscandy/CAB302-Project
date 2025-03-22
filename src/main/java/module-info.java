d981ebbb387918330a5914235aa948fdc522e0b3
Andrew Clarke <a40.clarke@connect.qut.edu.au>
2025-03-22 21:04:12 +1000

    chore(practical 4): Add complete working solution

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module com.cab302.cab302project {
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires java.desktop;
 
     opens com.cab302.cab302project to javafx.fxml;
     exports com.cab302.cab302project;
