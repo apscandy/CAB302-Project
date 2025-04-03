193ef6941dbad47431a161cbf40dd1fea97325c6
Hoang Dat Bui <davidbui0025@gmail.com>
2025-04-03 23:07:23 +1000

    add(feat/securityQuestion): Complete class UserSecurityQuestion

--- src/main/java/com/cab302/cab302project/model/userSecQuestions/UserSecurityQuestion.java
+++ src/main/java/com/cab302/cab302project/model/userSecQuestions/UserSecurityQuestion.java
@@ -1,4 +1,69 @@
 package com.cab302.cab302project.model.userSecQuestions;
 
+import com.cab302.cab302project.model.user.User;
+
 public class UserSecurityQuestion {
+    private final User user;
+    private String questionOne;
+    private String questionTwo;
+    private String questionThree;
+    private String answerOne;
+    private String answerTwo;
+    private String answerThree;
+
+    public UserSecurityQuestion(User user) {
+        this.user = user;
+    }
+
+    public String getQuestionOne() {
+        return questionOne;
+    }
+
+    public int getUserId() {
+        return user.getId();
+    }
+
+    public String getQuestionTwo() {
+        return questionTwo;
+    }
+
+    public String getQuestionThree() {
+        return questionThree;
+    }
+
+    public String getAnswerOne() {
+        return answerOne;
+    }
+
+    public String getAnswerTwo() {
+        return answerTwo;
+    }
+
+    public String getAnswerThree() {
+        return answerThree;
+    }
+
+    public String setQuestionOne(String questionOne) {
+        return this.questionOne = questionOne;
+    }
+
+    public String setQuestionTwo(String questionTwo) {
+        return this.questionTwo = questionTwo;
+    }
+
+    public String setQuestionThree(String questionThree) {
+        return this.questionThree = questionThree;
+    }
+
+    public String setAnswerOne(String answerOne) {
+        return this.answerOne = answerOne;
+    }
+
+    public String setAnswerTwo(String answerTwo) {
+        return this.answerTwo = answerTwo;
+    }
+
+    public String setAnswerThree(String answerThree) {
+        return this.answerThree = answerThree;
+    }
 }
