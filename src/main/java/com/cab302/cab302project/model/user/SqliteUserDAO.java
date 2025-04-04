c2acf8cb4c01d2aa4f571e11956ad1cae90cb790
Maverick Doan <144667093+sharkysondoan@users.noreply.github.com>
2025-04-04 11:17:58 +1000

    feat(users): Added database operation methods to SqliteUserDAO

--- src/main/java/com/cab302/cab302project/model/user/SqliteUserDAO.java
+++ src/main/java/com/cab302/cab302project/model/user/SqliteUserDAO.java
@@ -3,8 +3,10 @@ package com.cab302.cab302project.model.user;
 import com.cab302.cab302project.model.SqliteConnection;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
-public class SqliteUserDAO {
+public class SqliteUserDAO implements IUserDAO {
 
     private final Connection con;
 
@@ -34,4 +36,88 @@ public class SqliteUserDAO {
     private final String getUserByEmailSQL = """
             SELECT * FROM users WHERE email = ?;
             """;
+
+    public void  createUserTable() {
+        try {
+            PreparedStatement sql = con.prepareStatement(createUserTableSQL);
+            sql.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void addUser (User user) {
+        try {
+            PreparedStatement sql = con.prepareStatement(addUserSQL);
+            sql.setString(1, user.getFirstName());
+            sql.setString(2, user.getLastName());
+            sql.setString(3, user.getEmail());
+            sql.setString(4, user.getPassword());
+            sql.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void updateUser (User user) {
+        try {
+            PreparedStatement sql = con.prepareStatement(updateUserSQL);
+            sql.setString(1, user.getFirstName());
+            sql.setString(2, user.getLastName());
+            sql.setString(3, user.getEmail());
+            sql.setString(4, user.getPassword());
+            sql.setInt(5, user.getId());
+            sql.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public User getUser (int id) {
+        User user = null;
+        try {
+            PreparedStatement sql = con.prepareStatement(getUserByIdSQL);
+            sql.setInt(1, id);
+            ResultSet result = sql.executeQuery();
+            if (result.next()) {
+                user = new User (
+                        result.getInt("id"),
+                        result.getString("firstName"),
+                        result.getString("lastName"),
+                        result.getString("email"),
+                        result.getString("password")
+                );
+            }
+            result.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+    @Override
+    public User getUser(String email) {
+        User user = null;
+        try {
+            PreparedStatement sql = con.prepareStatement(getUserByEmailSQL);
+            sql.setString(1, email);
+            ResultSet result = sql.executeQuery();
+            if (result.next()) {
+                user = new User (
+                        result.getInt("id"),
+                        result.getString("firstName"),
+                        result.getString("lastName"),
+                        result.getString("email"),
+                        result.getString("password")
+                );
+            }
+            result.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
 }
