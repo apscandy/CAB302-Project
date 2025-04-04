43b8c7e56998fa862018385a8e8692e36744a52e
Maverick Doan <144667093+sharkysondoan@users.noreply.github.com>
2025-04-04 10:31:50 +1000

    feat(users): Added SQL queries for database operations related to user objects

--- src/main/java/com/cab302/cab302project/model/user/SqliteUserDAO.java
+++ src/main/java/com/cab302/cab302project/model/user/SqliteUserDAO.java
@@ -5,11 +5,33 @@ import com.cab302.cab302project.model.SqliteConnection;
 import java.sql.Connection;
 
 public class SqliteUserDAO {
+
     private final Connection con;
 
     public SqliteUserDAO() {
         con = SqliteConnection.getInstance();
     }
 
-
+    private final String createUserTableSQL = """
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                firstName TEXT NOT NULL,
+                lastName TEXT NOT NULL,
+                email TEXT NOT NULL UNIQUE,
+                password TEXT NOT NULL
+            );""";
+    private final String addUserSQL = """
+            INSERT INTO users (firstName, lastName, email, password)
+                VALUES (?, ?, ?, ?);
+            """;
+    private final String updateUserSQL = """
+            UPDATE users SET firstName = ?, lastName = ?, email = ?, password = ? 
+                WHERE id = ?;
+            """;
+    private final String getUserByIdSQL = """
+            SELECT * FROM users WHERE id = ?;
+            """;
+    private final String getUserByEmailSQL = """
+            SELECT * FROM users WHERE email = ?;
+            """;
 }
