7104f6ed037b90398cc2e11b8bc05d28741135e1
Dang Linh Phan <phandanglinh2005@gmail.com>
2025-04-05 20:23:36 +1000

    build(register): implement register  FXML view

--- src/main/java/com/cab302/cab302project/controller/RegisterController.java
+++ src/main/java/com/cab302/cab302project/controller/RegisterController.java
@@ -1,5 +1,10 @@
 package com.cab302.cab302project.controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
 public class RegisterController {
     
 }
