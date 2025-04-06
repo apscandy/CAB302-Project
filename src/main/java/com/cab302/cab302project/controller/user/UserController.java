edb04603c9b82d1463e97e69a3c46b6fbe36cde4
Maverick Doan <144667093+sharkysondoan@users.noreply.github.com>
2025-04-06 11:04:44 +1000

    feat(userController): Modified secuirty question check method and reset password method

--- src/main/java/com/cab302/cab302project/controller/user/UserController.java
+++ src/main/java/com/cab302/cab302project/controller/user/UserController.java
@@ -3,6 +3,8 @@ package com.cab302.cab302project.controller.user;
 import com.cab302.cab302project.ApplicationState;
 import com.cab302.cab302project.model.user.SqliteUserDAO;
 import com.cab302.cab302project.model.user.User;
+import com.cab302.cab302project.model.userSecQuestions.SqliteUserSecurityQuestionDAO;
+import com.cab302.cab302project.model.userSecQuestions.UserSecurityQuestion;
 
 public class UserController {
     public static boolean authenticate (String email, String password, SqliteUserDAO userDAO) {
@@ -33,13 +35,40 @@ public class UserController {
         return false;
     }
 
-    public static boolean resetPassword (String email, String newPassword, SqliteUserDAO userDAO) {
+    public boolean resetPassword (String email, String newPassword, SqliteUserDAO userDAO) {
         if (email.trim().isEmpty() || newPassword.trim().isEmpty()) {
             return false;
         }
         User checkUser = userDAO.getUser(email);
         if (checkUser != null) {
             checkUser.setPassword(newPassword);
+            userDAO.updateUser(checkUser);
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkSecurityQuestion (String email, String answerOne, String answerTwo, String answerThree,
+                                 SqliteUserSecurityQuestionDAO questionDAO, SqliteUserDAO userDAO) {
+        if (answerOne.trim().isEmpty() || answerTwo.trim().isEmpty() || answerThree.trim().isEmpty()
+        || email.trim().isEmpty()) {
+            return false;
+        }
+        User user = userDAO.getUser(email);
+        if (user == null) {
+            return false;
+        }
+        UserSecurityQuestion questions = questionDAO.getQuestions(user);
+        if (questions == null) {
+            return false;
+        }
+        boolean validAnswers = (
+                questions.getAnswerOne().equalsIgnoreCase(answerOne.trim()) &&
+                questions.getAnswerTwo().equalsIgnoreCase(answerTwo.trim()) &&
+                questions.getAnswerThree().equalsIgnoreCase(answerThree.trim())
+        );
+        if (validAnswers) {
+            return true;
         }
         return false;
     }
