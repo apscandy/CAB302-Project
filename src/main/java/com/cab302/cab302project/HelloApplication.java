3e82a29e38df236e19c284faf493ff6b60c9a4a3
Monica Borg <monicaborg@MacBookPro.gateway>
2025-04-09 09:14:49 +1000

    Adding access modifiers to ICardDAO

--- src/main/java/com/cab302/cab302project/HelloApplication.java
+++ src/main/java/com/cab302/cab302project/HelloApplication.java
@@ -17,7 +17,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("prompt-email-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main/main.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.resizableProperty().setValue(Boolean.FALSE);
         stage.setTitle(TITLE);
