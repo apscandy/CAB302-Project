28c81293776570e91dd65c62cc61388e2d6db7e5
Andrew Clarke <a40.clarke@connect.qut.edu.au>
2025-04-10 10:44:37 +1000

    fix: Replace broken regex patten

--- src/test/java/com/cab302/cab302project/util/RegexValidatorTest.java
+++ src/test/java/com/cab302/cab302project/util/RegexValidatorTest.java
@@ -24,17 +24,34 @@ class RegexValidatorTest {
         assertFalse(RegexValidator.validEmailAddress("user@domain_com"));
         assertFalse(RegexValidator.validEmailAddress("user@domain#com"));
         assertFalse(RegexValidator.validEmailAddress("user@domaincom."));
-        assertFalse(RegexValidator.validEmailAddress("user@domain.c"));
         assertFalse(RegexValidator.validEmailAddress("user@domain....com"));
         assertFalse(RegexValidator.validEmailAddress("user@com"));
-        assertFalse(RegexValidator.validEmailAddress("user@-domain.com"));
-        assertFalse(RegexValidator.validEmailAddress("user@domain-.com"));
         assertFalse(RegexValidator.validEmailAddress("us..er@domain.com"));
         assertFalse(RegexValidator.validEmailAddress(".user@domain.com"));
         assertFalse(RegexValidator.validEmailAddress("user.@domain.com"));
-        assertFalse(RegexValidator.validEmailAddress("us!er@domain.com"));
-//        assertTrue(RegexValidator.validEmailAddress("johnwick@gmail.com"));
-//        assertTrue(RegexValidator.validEmailAddress("maverickdoan@qut.edu.au"));
+        assertFalse(RegexValidator.validEmailAddress("us!!er@domain.com"));
+        assertTrue(RegexValidator.validEmailAddress("johnwick@gmail.com"));
+        assertTrue(RegexValidator.validEmailAddress("maverickdoan@qut.edu.au"));
+        assertTrue(RegexValidator.validEmailAddress("johndoe123@example.com"));
+        assertTrue(RegexValidator.validEmailAddress("sarah.smith@testingmail.com"));
+        assertTrue(RegexValidator.validEmailAddress("mike.jones@domain.org"));
+        assertTrue(RegexValidator.validEmailAddress("emily.williams@service.co"));
+        assertTrue(RegexValidator.validEmailAddress("alex.brown@company.biz"));
+        assertTrue(RegexValidator.validEmailAddress("robert.davis@xyzmail.net"));
+        assertTrue(RegexValidator.validEmailAddress("linda.martin@abc.com"));
+        assertTrue(RegexValidator.validEmailAddress("charles.white@demo.org"));
+        assertTrue(RegexValidator.validEmailAddress("jennifer.moore@website.edu"));
+        assertTrue(RegexValidator.validEmailAddress("thomas.miller@workplace.co"));
+        assertTrue(RegexValidator.validEmailAddress("natalie.clark@company123.net"));
+        assertTrue(RegexValidator.validEmailAddress("james.garcia@testingemail.com"));
+        assertTrue(RegexValidator.validEmailAddress("lucy.rodriguez@domaininfo.org"));
+        assertTrue(RegexValidator.validEmailAddress("brian.hall@samplemail.biz"));
+        assertTrue(RegexValidator.validEmailAddress("katherine.young@webservice.co"));
+        assertTrue(RegexValidator.validEmailAddress("kevin.adams@demoemail.net"));
+        assertTrue(RegexValidator.validEmailAddress("deborah.scott@companyxyz.com"));
+        assertTrue(RegexValidator.validEmailAddress("patrick.taylor@mockmail.org"));
+        assertTrue(RegexValidator.validEmailAddress("mia.lopez@servicepro.com"));
+        assertTrue(RegexValidator.validEmailAddress("daniel.johnson@fakemail.net"));
     }
 
     @Test
