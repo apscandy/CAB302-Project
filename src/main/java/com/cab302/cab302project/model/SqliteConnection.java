3d05422632a9d62726dfc50e4680a309ac84bf17
Andrew Clarke <a40.clarke@connect.qut.edu.au>
2025-04-10 20:26:48 +1000

    fix: Fix an issue with the PRAGAM not reporting right

--- src/main/java/com/cab302/cab302project/model/SqliteConnection.java
+++ src/main/java/com/cab302/cab302project/model/SqliteConnection.java
@@ -124,22 +124,23 @@ public class SqliteConnection {
         try {
             Statement stmt = instance.createStatement();
             ResultSet rsForeignKeys = stmt.executeQuery("PRAGMA foreign_keys;");
-            ResultSet rsSynchronous = stmt.executeQuery("PRAGMA synchronous;");
-            ResultSet rsLockingMode = stmt.executeQuery("PRAGMA locking_mode;");
             if (rsForeignKeys.next()) {
                 int foreignKeysEnabled = rsForeignKeys.getInt(1);
                 logger.debug("Foreign keys enabled: {}", foreignKeysEnabled);
             }
+            rsForeignKeys.close();
+            ResultSet rsSynchronous = stmt.executeQuery("PRAGMA synchronous;");
+
             if (rsSynchronous.next()) {
                 int synchronousMode = rsSynchronous.getInt(1);
                 logger.info("Synchronous Mode: {}", synchronousMode);
             }
+            rsSynchronous.close();
+            ResultSet rsLockingMode = stmt.executeQuery("PRAGMA locking_mode;");
             if (rsLockingMode.next()) {
                 String lockingMode = rsLockingMode.getString(1);
                 logger.info("Locking Mode: {}", lockingMode);
             }
-            rsForeignKeys.close();
-            rsSynchronous.close();
             rsLockingMode.close();
             stmt.close();
         } catch (Exception e){
@@ -147,3 +148,5 @@ public class SqliteConnection {
         }
     }
 }
+
+
