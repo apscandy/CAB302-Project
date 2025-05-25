2c250aea7614f77151a047f0320f7ef2d073616b
Maverick Doan <144667093+maverick-doan@users.noreply.github.com>
2025-05-25 17:01:23 +1000

    refactor(results): Moved result controller to a separated package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -25,6 +25,7 @@ module com.cab302.cab302project {
     opens com.cab302.cab302project.controller.user to javafx.fxml;
     opens com.cab302.cab302project.controller.card to javafx.fxml;
     opens com.cab302.cab302project.controller.recyclebin to javafx.fxml;
-    exports com.cab302.cab302project.controller;
     opens com.cab302.cab302project.controller to javafx.fxml;
+    exports com.cab302.cab302project.controller.results;
+    opens com.cab302.cab302project.controller.results to javafx.fxml;
 }
